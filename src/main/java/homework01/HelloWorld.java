975fbf854f1a830dcf0279e07131c46602abb42e
Savin Vladimir <8SavinVladimir8@gmail.com>
2020-09-12 15:01:52 +0300

    + homework 01 is ready

---
+++ src/main/java/homework01/HelloWorld.java
@@ -0,0 +1,13 @@
+package homework01;
+
+/**
+ * Класс {@code HelloWorld} выводит в консоль сообщение 'Hello world!!!'.
+ *
+ * @author Savin Vladimir
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
